8980d605fad85572f11b45a4250d2ab0fe3bca3f
Khalil Albattashi <albattashi.ka@gmail.com>
2023-06-18 13:13:50 +0400

    Extensions And LINQ Operators

---
+++ CsharpLearningJourney/Week-7/LINQ/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    /*
+     to make an extension you need a static class in the same name space and inside the class a public static
+     method to use the extension elsewhere.
+     */
+    static class Extension
+    {
+        //global extension for the int class
+        public static int mirror(this int x)
+        {
+            char[] y = x.ToString().ToCharArray();
+
+            Array.Reverse(y);
+            
+            return int.Parse(new string(y));  
+        }
+    }
+}
---
+++ CsharpLearningJourney/Week-7/LINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Extensions
+            int x = 1234;
+            int y = x.mirror();
+            //Console.WriteLine(y);
+
+            
+            // LINQ Operators different syntax
+            List<int> list = new List<int>() { 1,2,4,45,6,3,4,7,8,9,10};
+            // methods syntax
+            var EvenRes = list.Where(x => x % 2 == 0);
+            
+            // sql syntax
+            var OddRes = from i in list
+                         where i % 2 != 0
+                         select i;
+
+            // Result output
+            Console.WriteLine("Even Numbers");
+            foreach (var item  in EvenRes)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Odd Numbers");
+            foreach (var item in OddRes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
