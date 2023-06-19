a744db6a06a470f92c381272eb373ae00fdf1d5e
Khalil Albattashi <albattashi.ka@gmail.com>
2023-06-19 22:16:14 +0400

    Using Entity Framework to interact with the Database

---
+++ CsharpLearningJourney/Week-7/EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        //connecting to the database
+        // after connecting we perform migration to the database
+        // add-migration <name of the migration> command
+        // then add the updates to the database
+        // update-database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Company;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+        }
+
+        //adding table to the database
+        DbSet<Employee> Employees { set; get; }
+    }
+}
