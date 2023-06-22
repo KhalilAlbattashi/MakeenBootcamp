9840351f689e04e6f4db27ef4f9231801d1483cf
Khalil Albattashi <albattashi.ka@gmail.com>
2023-06-22 12:07:04 +0400

    Using Entity Framework to add relations inside the database

---
+++ CsharpLearningJourney/Week-7/EntityFrameworkRelations/Audit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore
+{
+    public class Audit
+    {
+        public int AuditID { get; set; }
+        public string AuditName { get; set; }
+        public Post AuditPost { get; set; }
+    }
+}
---
+++ CsharpLearningJourney/Week-7/EntityFrameworkRelations/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [Required]
+        public string PostTitle { get; set; }
+        [Required]
+        public string PostDescription { get; set; }
+        public string? PostTag { get; set; }
+        public List<Blog> PostPlogs{ get; set; }  
+    }
+}
