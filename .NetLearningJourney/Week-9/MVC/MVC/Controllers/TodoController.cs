90865a5969444bf65b7130fc108648a3b396e0f0
Khalil Albattashi <albattashi.ka@gmail.com>
2023-07-04 12:19:35 +0400

    Todo list Web page using Asp.Net MVC

---
+++ .NetLearningJourney/Week-9/MVC/MVC/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
